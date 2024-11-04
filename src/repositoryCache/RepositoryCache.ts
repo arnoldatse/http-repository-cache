46511105179381bff488f84ec65e099ce3ee1c91
Arnold Atsé <atse.arnold@gmail.com>
2024-11-04 12:54:13 -0500

    fix: :bug: used httpRequest buildRequestHeader method to get the real header built by the httpRequest to do request so that enhance the accuracy of identification of  cached request to use

--- src/repositoryCache/RepositoryCache.ts
+++ src/repositoryCache/RepositoryCache.ts
@@ -70,10 +70,11 @@ export default class RepositoryCache<O = unknown> {
   }
 
   private createRequestSignature<B>(method: HttpMethod, httpRequestParams: HttpRequestParams<B, O>): RequestSignature<B> {
+    const requestHeader = this.httpRequest.buildRequestHeader(httpRequestParams);
     return {
       method: method,
       ...(httpRequestParams.body && { body: httpRequestParams.body }),
-      ...(httpRequestParams.headers && { headers: httpRequestParams.headers })
+      ...(Object.keys(requestHeader).length > 0 && { headers: requestHeader })
     }
 
   }
