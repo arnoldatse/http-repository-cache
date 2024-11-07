0119f6ac2f255ca0a93afa5c741f0f3262b70791
Arnold Atsé <atse.arnold@gmail.com>
2024-11-07 14:55:12 -0500

    docs: :memo: wrote documentation summary and added MIT Licence

--- src/httpRequest/exception/HttpException.ts
+++ src/httpRequest/exception/HttpException.ts
@@ -3,7 +3,7 @@ import DefaultHttpExceptionType from "./DefaultHttpExceptionType";
 /**
  * Represents an HTTP exception.
  *
- * @template T - The type of the additional possible exception type.
+ * @template T - The additional possible exception type.
  * @template B - The type of the body of the exception.
  */
 export default interface HttpException<T = unknown, B = unknown> {
